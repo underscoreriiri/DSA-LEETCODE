7a2c077945f58e703147fd9822b217265307df80
Riya Singh <riyasinghrathore12@gmail.com>
2026-03-16 23:28:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (10.16%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            mp[t[i]]--;
+
+            if(mp[t[i]]<0) return false;
+        }
+        return true;
+    }
+};
