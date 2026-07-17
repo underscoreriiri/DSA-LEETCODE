c151c2ccddccf53e0307720e1bd43c973c65dc89
Riya Singh <riyasinghrathore12@gmail.com>
2026-07-17 12:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (87.45%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int low=0,high=x,ans=-1;
+        while(low<=high){
+        int mid=low+ (high-low)/2;
+        long long square = 1LL * mid * mid;
+        if(square==x){
+            return mid;
+        }
+        else if(square<x){
+            ans=mid;
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
         
+        }
+        return ans;
     }
 };
